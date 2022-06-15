0c515cd23a530f24c8a9a29fb1e6f9f1aa0ab5a3
AG-cpu <ahmadefatehy@gmail.com>
2022-06-15 02:26:08 -0700

    146Ssprojref

--- shell.h
+++ shell.h
@@ -14,12 +14,6 @@
 #define END_OF_FILE -2
 #define EXIT -3
 
-/* Global environemnt */
-extern char **environ;
-/* Global program name */
-char *name;
-/* Global history counter */
-int hist;
 
 /**
  * struct struc_list -  linked list.
@@ -128,6 +122,13 @@ void help_setenv(void);
 void help_unsetenv(void);
 void help_history(void);
 int files_com_hndler(char *file_path, int *lst_com);
+
+/* environemnt */
+extern char **environ;
+/* program name */
+char *name;
+/* history counter */
+int hist;
 
 
 #endif
